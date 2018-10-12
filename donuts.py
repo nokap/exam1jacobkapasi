8dd358d9c4899f300d3d997e94cf65e7046d1c12
Jacob Kapasi <2020jkapasi@dwight.edu>
2018-10-12 09:11:28 -0400

    init

---
+++ donuts.py
@@ -0,0 +1,26 @@
+grades = [62, 79, 82, 81, 92, 74, 84, 95, 85, 78, 88]
+#This is an array that holds all of the grades of the class
+ans = () #This is a variable that holds the answer
+for avg in grades:    #I am creating a for loop that holds the averages for the grades
+    if avg ==:   #I am saying that if the average variable in grades is equal to... (the stuff below)
+        A_avg == int >= 90  #if the average is equal to or above 90, the average is an A
+        B_avg == int >= 80 < 90 #if the average is equal to or above 90 and less than 90, the avg is B
+        C_avg == int >= 70 < 80 #if the average is equal to or above 70 and less than 80, the avg is C
+        D_avg == int >= 60 < 70 #if the average is equal to or above 60 and less than 70, the avg is D
+        F_avg == int >= 50 < 60 #if the average is equal to or above 50 and less than 60, the avg is F
+
+avg = sum(grades)/11 #The sum of the grades divided by 11 is the average of the class
+
+elif avg == A_avg: #If the average is equal to an A then Mr. James james will give a full donut to the students.
+    print('Yay! Mr. james will give you a full donut')
+elif avg == B_avg: #If the average is equal to a B, then Mr. James will give half a donut to the students.
+    print('Room for improvement, but you still did well! Mr. James will give you half a donut')
+elif avg == C_avg: #If the average is equal to a C, then Mr. James will give 1/3 a donut to the students.
+    print('You really need to step it up, but you have a start, Mr. James will give you 1/3 of a donut')
+elif avg == D_avg: #If the average is equal to a D, then Mr. James will get half a donut from each student.
+    print('Wow, did you gain anything from this course? You owe Mr. James half a donut!')
+return #I am trying to call the function so I get the answer
+ans
+
+
+# Step 3: I have a bunch of errors with my code, I was told that I can clean up my code so that it runs properly. 
---
+++ kenobi.py
@@ -0,0 +1,25 @@
+#Hi class, my name is Jake! Today I am going to be teaching you about variables, arrays, if else statements, and for loopsself.
+
+#I know that sounds like a lot of information to learn at once, especially for someone who is just starting outself.
+#Despite that, I promise I will do my best to give you guys the information you needself.
+
+#STEP 1: variables
+ # A variable in math class is something that is generally unknown, like the value x, that has a value we have to find.
+ # In computer science, a variable is used to make code consice, so that your page isn't filled with tons and tons of code.
+#Here is our first variableself.
+
+a = (10+10)/2  #In here we have made a 'a' equal to '=' ten plus ten in parenthesis (10+10) divded by 2 (/2)
+print(a) #I type "print(a) so that when I input this program in terminal, the answer pops out"
+
+
+
+#STEP 2: arrays
+
+#Arrays are a list of value, they may look scary, but they are really helpful and simple to type outself.
+
+arrexample = ['jake', 'jon', 'malachy']
+#this is an example of an array. In brackets it stores separate values that can be printed out as a list.
+#an array name is a variable in that it can be named absoluetly anything as long as it has brackets, and separate terms. Think of it as a list.
+#When I print out this arr, what do you think is going to pop out?
+print(arrexample)
+# This prints out the array.
